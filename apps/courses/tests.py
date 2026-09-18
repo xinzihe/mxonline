fc0c7fcab19b7d3f465b4d03f0363c0cd0404811
xinzihe <1735331467@qq.com>
2026-09-18 17:08:47 +0800

    修改公开课程、授课机构、授课教师没有数据的时候报错

--- apps/courses/tests.py
+++ apps/courses/tests.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.models import AnonymousUser
+from django.template.loader import render_to_string
+from django.test import RequestFactory
 from django.test import SimpleTestCase
 from django.urls import reverse
+from pure_pagination import Paginator
 
 
 class CourseUrlTests(SimpleTestCase):
@@ -7,3 +11,20 @@ class CourseUrlTests(SimpleTestCase):
         self.assertEqual(reverse('course:course_list'), '/course/list/')
         self.assertEqual(reverse('course:course_detail', args=[1]), '/course/detail/1/')
         self.assertEqual(reverse('course:video_play', args=[1]), '/course/video/1/')
+
+    def test_course_list_renders_when_no_courses_exist(self):
+        request = RequestFactory().get('/course/list/')
+        request.user = AnonymousUser()
+        empty_page = Paginator([], 12, request=request).page(1)
+
+        html = render_to_string('course-list.html', {
+            'all_courses': empty_page,
+            'course_nums': 0,
+            'hot_courses': [],
+            'sort': '',
+            'category': '',
+            'degree': '',
+            'keywords': '',
+        }, request=request)
+
+        self.assertIn('未找到符合条件的课程', html)
--- apps/organization/tests.py
+++ apps/organization/tests.py
@@ -28,3 +28,18 @@ class OrganizationUrlTests(SimpleTestCase):
         }, request=request)
 
         self.assertIn('未找到符合条件的授课机构', html)
+
+    def test_teacher_list_renders_when_no_teachers_exist(self):
+        request = RequestFactory().get('/org/teacher/list/')
+        request.user = AnonymousUser()
+        empty_page = Paginator([], 10, request=request).page(1)
+
+        html = render_to_string('teachers-list.html', {
+            'all_teachers': empty_page,
+            'teacher_nums': 0,
+            'sorted_teachers': [],
+            'sort': '',
+            'fav_teacher_ids': set(),
+        }, request=request)
+
+        self.assertIn('暂无入驻讲师', html)
